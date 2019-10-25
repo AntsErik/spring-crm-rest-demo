ae5183947c898520890c4b4f8cc4f89ed355fa43
AntsErik <ants.erik.noormagi@aweroc.com>
2019-10-25 09:22:10 +0300

    Creating Customer Error Response class and Custom Exception class.

---
+++ src/main/java/ee/praktika/springdemo/rest/CustomerNotFoundException.java
@@ -0,0 +1,24 @@
+package ee.praktika.springdemo.rest;
+
+public class CustomerNotFoundException extends RuntimeException {
+
+    public CustomerNotFoundException() {
+    }
+
+    public CustomerNotFoundException( String message ) {
+        super( message );
+    }
+
+    public CustomerNotFoundException( Throwable cause ) {
+        super( cause );
+    }
+
+    public CustomerNotFoundException( String message, Throwable cause ) {
+        super( message, cause );
+    }
+
+    public CustomerNotFoundException( String message, Throwable cause, boolean enableSupression, boolean writableStackTrace ) {
+        super( message, cause, enableSupression, writableStackTrace );
+    }
+
+}
